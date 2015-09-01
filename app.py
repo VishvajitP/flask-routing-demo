7dd3aaea808b910e438ddf11480e7f9157224c77
Vishvajit Pathak <vishvajit.pathak@incentius.com>
2015-09-01 15:16:44 +0530

    added basic rules of routing in flask

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+app = Flask(__name__)
+
+# we use route() decorator to tell flask what URL should trigger out function
+
+@app.route('/')         # without explicitly mentioning the end point
+def hello_world():
+    return '<h1> Index Page! </h1>'
+
+@app.route('/hello')  # explicitly mentioning the end point
+def view_function_hello():
+    return 'End Point With Hello'
+
+@app.route('/demo/<end_point>') # variable end point
+def show_user_profile(username):
+    return 'Your end point is :  <br> /demo/%s' % end_point
+
+@app.route('/post/<int:post_id>') # variable end point with given data type ; like- int, float, path
+def show_post(post_id):
+    return 'Post %d' % post_id
+
+# Trailing slash dilemma with flask URLs
+@app.route('/with_trailing_slash/')
+def with_slash():
+    return 'with_trailing_slash'
+
+@app.route('/without_trailing_slash')
+def without_slash():
+    return 'without_trailing_slash'
+'''
+if trailing slash is mentioned then you can access this end point irrespective of trailing slash mentioned in the request.
+In the second case, however, the URL is defined without a trailing slash, rather like the pathname of a file on UNIX-like systems. Accessing the URL with a trailing slash will produce a 404 “Not Found” error.
+'''
+
+
+if __name__ == '__main__':
+    app.run()
